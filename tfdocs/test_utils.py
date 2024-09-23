9c7ee9e68c1dd504dc03dc7bc8b283d0302194fb
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2024-09-23 14:22:11 +0100

    feat: add code testing (#14)
    * feat: add code testing
    
    * feat: add testing workflow

---
+++ tfdocs/test_utils.py
@@ -0,0 +1,10 @@
+from tfdocs.utils import hello_world
+import pytest
+
+@pytest.mark.parametrize("case, exp", [
+	("world", "hello, world!"),
+	("test", "hello, test!"),
+	("", "hello, world!"),
+])
+def test_hello_world(case, exp):
+	assert hello_world(case) == exp
