bc1a8f284e977120450c642ffeb89b96265772f9
acrease <alex@crease.sh>
2024-09-26 02:03:20 +0100

    fix: move logging for which command is being run

--- tfdocs/__init__.py
+++ tfdocs/__init__.py
@@ -4,14 +4,16 @@ import logging
 
 def main():
     parser, args = parse_args()
+
     setup_logs(
         print_log_level=args["verbose"],
         enable_log_streaming=args["serve_logs"]
     )
     log = logging.getLogger(__name__)
-    log.info(f"Running-subcommand {args["command"]}")
 
     if 'func' in args:
+        log.info(f"Running command {args["command"]}")
         args["func"]()
     else: 
         parser.print_help()
+        exit(0)
