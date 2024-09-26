e952f0ce40fbdbbb14f7a1f1253087a8cde9f604
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2024-09-26 01:17:07 +0100

    feat: Configure infrastructure basics (#19)
    * feat: setup remote state backend and be mgr
    
    * feat: added infrastructure ci jobs
    
    * fix: added permissions
    
    * fix: properly stagger workflows
    
    * fix: new s3 user var and fix typo in workflow
    
    * fix: terraform plan pr number

---
+++ env.auto.tfvars
@@ -0,0 +1,4 @@
+apply    = "true"
+env      = "dev"
+name     = "dev"
+role_arn = "arn:aws:iam::013948180024:role/tfdocs-iac-role"
---
+++ envs/backend_manager.tf
@@ -0,0 +1,31 @@
+variable "ENV" {
+  description = "the name of the environment to load"
+  default     = "dev"
+}
+
+module "backend" {
+  source          = "Apollo-XIV/backend-manager/aws"
+  version         = "0.0.28"
+  prefix          = "tfdocs"
+  force_destroy   = false
+  enable_dynamodb = false
+
+  output_dir = "../."
+
+  environment_configs_dir = abspath("${path.root}")
+  environments = [
+    "dev",
+    "staging"
+  ]
+  ENV = var.ENV
+
+  variables = {
+    name  = "string"
+    apply = "bool"
+  }
+
+  approved_arns = [
+    "arn:aws:iam::013948180024:user/desktop",
+    "arn:aws:iam::013948180024:user/github"
+  ]
+}
