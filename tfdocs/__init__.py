7b6f5e73552dc5237a2b3641474086f699d21b86
acrease <alex@crease.sh>
2024-09-26 01:21:50 +0100

    feat: add log streaming toggle

--- tfdocs/__init__.py
+++ tfdocs/__init__.py
@@ -6,7 +6,7 @@ def main():
     args = parse_args()
     setup_logs(
         print_log_level=args["verbose"],
-        enable_log_streaming=True
+        enable_log_streaming=args["serve_logs"]
     )
     log = logging.getLogger(__name__)
     log.debug("test message")
--- tfdocs/cli.py
+++ tfdocs/cli.py
@@ -1,12 +1,17 @@
 import argparse
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="A modular argparse command processor.")
+    parser = argparse.ArgumentParser(description="Terraform Documentation in the Terminal")
     parser.add_argument(
         "-v", "--verbose", action="count", default=0, help="Increase output verbosity (-v, -vv)"
     )
+    parser.add_argument(
+        "--serve-logs", action="store_true", default=False, help="Send logs to log viewing server"
+    )
 
     args = vars(parser.parse_args())
+
+    # make sure verbosity is in the correct range and prepare for logging module
     if args["verbose"] not in range(0,3):
         raise argparse.ArgumentError(None, "Incorrect number of 'verbose' flags applied")
     args["verbose"] = 30 - 10 * args["verbose"]
