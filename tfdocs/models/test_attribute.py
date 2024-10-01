d4bde2d422f2efc5fbfe6100ab0c74ba1cb0fd3a
acrease <alex@crease.sh>
2024-10-01 13:54:52 +0100

    test: commit

--- tfdocs/models/test_attribute.py
+++ tfdocs/models/test_attribute.py
@@ -55,39 +55,39 @@ def test_attribute_as_record():
     assert result == expected_result
 
 
-# def test_late_properties():
-#     attribute = MockAttribute(attribute_id="1")
-#     exp = MockAttribute(
-#         attribute_name="id",
-#         attribute_type=tf.String(),
-#         description="This is set to a random value at create time.",
-#         description_type=tf.DescType.PLAIN,
-#         optional=False,
-#         computed=True,
-#         block_id="83043286b6fc716303ed1484f849579d",
-#     )
-#     assert exp.name == attribute.name
-#     assert exp.type == attribute.type
-#     assert exp.description == attribute.description
-#     assert exp.description_type == attribute.description_type
-#     assert exp.computed == attribute.computed
-#     assert exp.optional == attribute.optional
-#     assert exp.block_id == attribute.block_id
+def test_late_properties():
+    attribute = MockAttribute(attribute_id="1")
+    exp = MockAttribute(
+        attribute_name="id",
+        attribute_type=tf.String(),
+        description="This is set to a random value at create time.",
+        description_type=tf.DescType.PLAIN,
+        optional=False,
+        computed=True,
+        block_id="83043286b6fc716303ed1484f849579d",
+    )
+    assert exp.name == attribute.name
+    assert exp.type == attribute.type
+    assert exp.description == attribute.description
+    assert exp.description_type == attribute.description_type
+    assert exp.computed == attribute.computed
+    assert exp.optional == attribute.optional
+    assert exp.block_id == attribute.block_id
 
-#     assert exp == attribute
+    assert exp == attribute
 
 
-# def test_repr():
-#     attribute = MockAttribute(
-#         attribute_id="1",
-#         attribute_name="id",
-#         attribute_type=tf.String(),
-#         description="This is set to a random value at create time.",
-#         description_type=tf.DescType.PLAIN,
-#         optional=True,
-#         computed=True,
-#     )
-#     assert (
-#         repr(attribute)
-#         == "[1/83-9d] id : string = 'This is set to a...' : plain (optional, computed)"
-#     )
+def test_repr():
+    attribute = MockAttribute(
+        attribute_id="1",
+        attribute_name="id",
+        attribute_type=tf.String(),
+        description="This is set to a random value at create time.",
+        description_type=tf.DescType.PLAIN,
+        optional=True,
+        computed=True,
+    )
+    assert (
+        repr(attribute)
+        == "[1/83-9d] id : string = 'This is set to a...' : plain (optional, computed)"
+    )
