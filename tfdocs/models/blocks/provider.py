9ace8712e50fe950137560e52087d54ae2afd47a
acrease <alex@crease.sh>
2024-10-22 16:45:19 +0100

    fix: added new blank files

---
+++ tfdocs/models/blocks/provider.py
@@ -0,0 +1,21 @@
+
+from tfdocs.models.block import Block
+
+
+
+class Provider(Block):
+    @classmethod
+    def from_name(cls, name: str) -> "Provider":
+        """
+        Returns the named provider as an object
+        """
+        res = cls._db.sql(
+            """
+             SELECT block_id, block_name FROM block 
+                 WHERE block_type == 'Provider' 
+                 AND block_name == ?;                    
+         """,
+            (name,),
+        ).fetchone()
+        new_obj = Provider(hash=res[0], name=res[1])
+        return new_obj
