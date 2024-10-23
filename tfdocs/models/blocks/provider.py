00c8fa61d05a2f4f55b40fe9bfa32cbb57d0871f
acrease <alex@crease.sh>
2024-10-23 20:23:56 +0100

    feat: add list all method to list all blocks in the collection

--- tfdocs/models/blocks/provider.py
+++ tfdocs/models/blocks/provider.py
@@ -6,9 +6,9 @@ log = logging.getLogger()
 
 class Provider(Block):
     @classmethod
-    def list_all(cls) -> list["Provider"]:
+    def list_providers(cls) -> list["Provider"]:
         """
-        Returns the named provider as an object
+        Returns all providers in the cache as objects with prefetched names
         """
         res = cls._db.sql(
             """
@@ -35,3 +35,23 @@ class Provider(Block):
         ).fetchone()
         new_obj = Provider(type="Provider", hash=res[0], name=res[1])
         return new_obj
+
+    def list_all(self):
+        """
+        Lists all Resources and Data Sources belonging to the provider
+        """
+        res = self._db.sql(
+            """
+            SELECT block_id, block_name, block_type FROM block
+            WHERE block_type IN ('Resource', 'DataSource')
+            AND parent_id == ?;
+        """,
+            (self.id,),
+        ).fetchall()
+        return [Block(type=a[2], hash=a[0], name=a[1]) for a in res]
+
+    def list_resources(self):
+        pass
+
+    def list_data_sources(self):
+        pass
--- tfdocs/models/blocks/test_provider.py
+++ tfdocs/models/blocks/test_provider.py
@@ -24,11 +24,74 @@ def test_from_name(case, exp):
     assert subject.id == exp
 
 
-def test_list_all():
+def test_list_providers():
     exp = {
         "registry.terraform.io/hashicorp/archive",
         "registry.terraform.io/hashicorp/null",
         "registry.terraform.io/hashicorp/time",
     }
-    subjects = MockProvider.list_all()
+    subjects = MockProvider.list_providers()
     assert {s.name for s in subjects} == exp
+
+
+@pytest.mark.parametrize(
+    "case, exp",
+    {
+        "85c7eb5ef45cc843ad01660e75695dce": {  # hashicorp/time's ID
+            "79308e46475d463658c5e9bcd37ccfe2",  # time_offset Resource
+            "1f3b97524b1efacbbd2b77679940710d",  # time_rotating Resource
+            "8cbde6725c88f9474cf8acaf239114c7",  # time_sleep Resource
+            "8f26c13e667a269ac45e8cd1909aaebf",  # time_static Resource
+        },
+        "3c09cf2d1f63e6886c1ff5bd2a9fa49d": {  # hashicorp/archive's ID
+            "d56975289b6c16e3e49a10e308ef58c3",  # archive_file Resource
+            "743e19765c5244e08afe83dca406e244",  # archive_file DataSource
+        },
+        "b68b2e47df542636676cf4c527b75aa0": {  # hashicorp/null's ID
+            "e832cce309a19eb2d126e41ac2f54997",  # null_resource Resource
+            "48052ad9e3ae785a9030b2c8628b6b54",  # null_data_source DataSource
+        },
+    }.items(),
+)
+def test_list_all(case, exp):
+    subjects = MockProvider(type="Provider", hash=case).list_all()
+    assert {s.id for s in subjects} == exp
+
+
+@pytest.mark.parametrize(
+    "case, exp",
+    {
+        "85c7eb5ef45cc843ad01660e75695dce": {  # hashicorp/time's ID
+            "79308e46475d463658c5e9bcd37ccfe2",  # time_offset Resource
+            "1f3b97524b1efacbbd2b77679940710d",  # time_rotating Resource
+            "8cbde6725c88f9474cf8acaf239114c7",  # time_sleep Resource
+            "8f26c13e667a269ac45e8cd1909aaebf",  # time_static Resource
+        },
+        "3c09cf2d1f63e6886c1ff5bd2a9fa49d": {  # hashicorp/archive's ID
+            "d56975289b6c16e3e49a10e308ef58c3",  # archive_file Resource
+        },
+        "b68b2e47df542636676cf4c527b75aa0": {  # hashicorp/null's ID
+            "e832cce309a19eb2d126e41ac2f54997",  # null_resource Resource
+        },
+    }.items(),
+)
+def test_list_resources(case, exp):
+    # TODO: implement resource block
+    pass
+
+
+@pytest.mark.parametrize(
+    "case, exp",
+    {
+        "85c7eb5ef45cc843ad01660e75695dce": {},  # hashicorp/time's ID
+        "3c09cf2d1f63e6886c1ff5bd2a9fa49d": {  # hashicorp/archive's ID
+            "743e19765c5244e08afe83dca406e244",  # archive_file DataSource
+        },
+        "b68b2e47df542636676cf4c527b75aa0": {  # hashicorp/null's ID
+            "48052ad9e3ae785a9030b2c8628b6b54",  # null_data_source DataSource
+        },
+    }.items(),
+)
+def test_list_data_sources(case, exp):
+    # TODO: implement data source block
+    pass
