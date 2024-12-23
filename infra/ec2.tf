7c76d7fa68afbfd00a46cf8a94e82bd8456ec31e
acrease <alex@crease.sh>
2024-12-23 01:18:19 +0000

    fix: typo in readme

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,23 +1,5 @@
 #------------- ASG Resources
 
-locals {
-  nixos_ami = "ami-0e7d1823ac80520e6"
-}
-
-data "aws_ami" "nixos" {
-  owners      = ["427812963091"]
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["nixos/24.11*"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
 data "aws_ami" "ubuntu_latest" {
   most_recent = true
 
@@ -88,14 +70,6 @@ data "cloudinit_config" "base" {
     })
   }
 
-  part {
-    content_type = "text/x-shellscript"
-    content      = <<-EOF
-      cloudwatch_agent_dl_url="https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb"
-      wget "$cloudwatch_agent_dl_url"
-      sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
-    EOF
-  }
 
   part {
     content_type = "text/x-shellscript"
@@ -118,17 +92,21 @@ data "cloudinit_config" "base" {
       chown -R webuser:webuser /var/www/tfdocs
 
       cd /var/www/tfdocs
-      # Start the webserver on :8000
-      # sudo -u webuser nohup \
-      #   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh; \
-      #   nix develop .#web \
-      #   gunicorn -w 4 --bind 0.0.0.0:8000 web:app &
 
       sudo -u webuser nohup bash -c '
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh; \
         nix develop .#web --command \
         gunicorn -w 4 --bind 0.0.0.0:8000 web:app \
       ' &
+    EOF
+  }
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = <<-EOF
+      cloudwatch_agent_dl_url="https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb"
+      wget "$cloudwatch_agent_dl_url"
+      sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
     EOF
   }
 }
@@ -149,6 +127,9 @@ resource "aws_autoscaling_group" "site" {
   instance_refresh {
     strategy = "Rolling"
   }
+
+  health_check_grace_period = 600
+  health_check_type         = "ELB"
 }
 
 resource "aws_security_group" "site" {
